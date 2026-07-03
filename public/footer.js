b7af492c7ddf08358e1cd2b2d9a19aa3a17e1fc7
bcwn-kdt <blackcatwhitenight@gmail.com>
2026-07-04 02:53:45 +0700

    Bump footer stylesheet version

--- public/footer.js
+++ public/footer.js
@@ -3,7 +3,7 @@
     if (document.querySelector('link[href^="/footer.css"]')) return;
     const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "/footer.css?v=footer-v1";
+    link.href = "/footer.css?v=footer-v2";
     document.head.appendChild(link);
   };
 
